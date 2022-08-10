58ace652e9e8c7863b759a9f4d0f29e49e5077b0
uurha <uurh2man@gmail.com>
2022-08-10 21:15:48 +0200

    Fix losing reference to scene asset

--- Assets/SceneManagement/Runtime/LoadSceneOptions.cs
+++ Assets/SceneManagement/Runtime/LoadSceneOptions.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 namespace SceneManagement.Runtime
 {
+    public delegate void SceneLoaderProgressChanged(float value);
+
     /// <summary>
     /// Loading Scene Options
     /// </summary>
@@ -12,5 +14,7 @@ namespace SceneManagement.Runtime
         public LoadSceneMode SceneLoadMode { get; set; } = LoadSceneMode.Additive;
         public UnloadSceneOptions SceneUnloadMode { get; set; } = UnloadSceneOptions.UnloadAllEmbeddedSceneObjects;
         public bool UseIntermediate { get; set; } = true;
+
+        public SceneLoaderProgressChanged ProgressChanged { get; set; } = null;
     }
 }
