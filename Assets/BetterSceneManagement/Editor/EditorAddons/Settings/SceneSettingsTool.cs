74a13e2928423dcc45050c3135e29d069bd8a8c0
uurhaa <uurha.dev@gmail.com>
2023-06-15 18:20:41 +0200

    Split classes into files

---
+++ Assets/BetterSceneManagement/Editor/EditorAddons/Settings/SceneSettingsTool.cs
@@ -0,0 +1,15 @@
+﻿using Better.EditorTools.SettingsTools;
+using Better.SceneManagement.Runtime;
+
+namespace Better.SceneManagement.EditorAddons.Settings
+{
+    internal class SceneSettingsTool : BetterSettingsTools<SceneLoaderSettings>
+    {
+        public const string SettingMenuItem = "Scene Management";
+        public const string MenuItemPrefix = BetterSettingsRegisterer.BetterPrefix + "/" + SettingMenuItem;
+        
+        public SceneSettingsTool() : base(nameof(SceneManagement), SettingMenuItem)
+        {
+        }
+    }
+}
